e5ad534bd3b9ffd7631d28fef909831a8b0e1ffc
Daniel Hernandez <dhernandez43@cnm.edu>
2019-10-20 13:52:31 -0600

    note: redid compser install on putty after readding json file

--- php/lib/author.php
+++ php/lib/author.php
@@ -6,7 +6,7 @@ use Ramsey\Uuid\Uuid;
 
 	$danny = new Author("f7286cc1-5d88-4a91-844e-588cbb940c67",
 	"f6cac5f10e4d14bf1cf85cfec2a0a24c",
-	"http://i.imgur.com/eBa3VXG.jpg",
+	"https://creativeimagelicensing.com/how-to-find-non-copyrighted-pictures/",
 	"audialb@yahoo.com",
 	"4a21312be53b16e88c5f78bcfddd16058f773cbc15ead7985b4dcb967dc85c698fcd080fe5ff93a9399c2003084abc3d8",
 	"audialb"
@@ -18,6 +18,3 @@ echo($danny->getAuthorAvatarUrl());
 echo($danny->getAuthorEmail());
 echo($danny->getAuthorHash());
 echo($danny->getAuthorUsername());
-
-
-
