28dc9d544a470687e89c4b903ea802da197f0f37
Daniel Hernandez <dhernandez43@cnm.edu>
2019-10-26 22:55:01 -0600

    ctrl shift f

--- php/lib/author.php
+++ php/lib/author.php
@@ -1,16 +1,17 @@
 <?php
 
-	require_once('../Classes/Author.php');
+require_once('../Classes/Author.php');
+
 use DanielCoderHernandez\ObjectOriented\Author;
 use Ramsey\Uuid\Uuid;
 
-	$danny = new Author("f7286cc1-5d88-4a91-844e-588cbb940c67",
+$danny = new Author("f7286cc1-5d88-4a91-844e-588cbb940c67",
 	"f6cac5f10e4d14bf1cf85cfec2a0a24c",
 	"https://creativeimagelicensing.com/how-to-find-non-copyrighted-pictures/",
 	"audialb@yahoo.com",
 	"4a21312be53b16e88c5f78bcfddd16058f773cbc15ead7985b4dcb967dc85c698fcd080fe5ff93a9399c2003084abc3d8",
 	"audialb"
-	);
+);
 
 echo($danny->getAuthorId());
 echo($danny->getAuthorActivationToken());
